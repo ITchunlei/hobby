10051efda3b93348e8eb2ed31187f98b00429af2
chunlei <ITchunlei@163.com>
2018-11-28 20:49:15 +0800

    test

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -219,7 +219,7 @@ void test() {
 
 class Kernel {
 public:
-    void Start();
+    virtual void Start();
     void Loop();
 };
 
@@ -245,7 +245,7 @@ void Kernel::Start() {
     tss_t* tss = &(tss_table[0]);
     tss->rsp0 = (uintptr_t)(&thread->context) - sizeof(context_t);
 
-    restore_context(&thread->context);
+    //restore_context(&thread->context);
 
 }
 
